4dc04cb48fa97d16e08bc4182e9ba4ec2be8fc85
Nathan Buckner <bucknath@amazon.com>
2018-04-18 15:51:33 -0700

    Fix docker horrors

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,4 @@ FROM python:2-alpine3.7
 WORKDIR /root
 RUN pip install falcon requests six
 COPY forward.py /root
+CMD ["python","/root/forward.py"]
--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-sudo docker build -t proxy .
-sudo docker run -d --restart always  -p 127.0.0.1:8000:8000 --name proxy --env-file env.in proxy python /root/forward.py
-echo '
-BASIC_USERNAME=
+docker build -t proxy .
+docker run -it --rm  -p 127.0.0.1:8000:8000 --name proxy --env-file env.in proxy
+echo 'BASIC_USERNAME=
 BASIC_PASS=
 DEST_URL=
 ' > env.in
