1efe89f7c84b23ee097c5f6f1ef644788e7dd710
Nathan Buckner <bucknerns@gmail.com>
2018-07-05 16:51:42 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t proxy .
-docker run -d --restart always -p 127.0.0.1:8000:8000 --name proxy --env-file env.in proxy
+docker run -d --restart always -p 172.17.0.1:8000:8000 --name proxy --env-file env.in proxy
 echo "don't forget to delete you env.in"
